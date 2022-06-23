bef855eb40532fe9c9ec0696c3e4d257bba93091
codyhenry <codyhenry527@gmail.com>
2022-06-23 11:10:05 -0400

    SideNav bar complete

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,22 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import {MatToolbarModule} from '@angular/material/toolbar';
-import {MatIconModule} from '@angular/material/icon';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import {MatMenuModule} from '@angular/material/menu';
-import {MatBadgeModule} from '@angular/material/badge';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatMenuModule } from '@angular/material/menu';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatDividerModule } from '@angular/material/divider';
+import {MatListModule } from '@angular/material/list';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {FlexLayoutModule} from '@angular/flex-layout';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
 import { BoardComponent } from './components/board/board.component';
 import { BoardCardsComponent } from './components/board-cards/board-cards.component';
 import { SidenavComponent } from './components/sidenav/sidenav.component';
+import { WorkspaceComponent } from './components/workspace/workspace.component';
 
 
 @NgModule({
@@ -22,7 +25,8 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     NavbarComponent,
     BoardComponent,
     BoardCardsComponent,
-    SidenavComponent
+    SidenavComponent,
+    WorkspaceComponent
   ],
   imports: [
     BrowserModule,
@@ -33,7 +37,9 @@ import { SidenavComponent } from './components/sidenav/sidenav.component';
     MatButtonModule,
     MatSidenavModule,
     MatMenuModule,
-    MatBadgeModule
+    MatBadgeModule,
+    MatDividerModule,
+    MatListModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/components/sidenav/sidenav.component.ts
+++ src/app/components/sidenav/sidenav.component.ts
@@ -7,6 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SidenavComponent implements OnInit {
 
+  userRank: string = 'Admin'; //get from db match userId with rank in board
   constructor() { }
 
   ngOnInit(): void {
